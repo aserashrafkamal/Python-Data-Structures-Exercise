5f5b00104f81b77bed5ddc651a1b25280bac6746
aserashrafkamal <150065919+aserashrafkamal@users.noreply.github.com>
2023-12-24 16:21:54 -0500

    Add files via upload

---
+++ python-ds-practice/fs_5_read_file_list/read_file_list.py
@@ -0,0 +1,24 @@
+def read_file_list(filename):
+    """Read file and print out each line separately with a "-" before it.
+
+    For example, if we have a file, `dogs`, containing:
+        Fido
+        Whiskey
+        Dr. Sniffle
+
+    This should work:
+
+        >>> read_file_list("dogs")
+        - Fido
+        - Whiskey
+        - Dr. Sniffle
+
+    It will raise an error if the file cannot be found.
+    """
+
+    with open(filename) as f:
+        for line in f:
+            # Remove the newline character from the end of the line.
+            line = line.strip()
+            print(f"- {line}")
+
